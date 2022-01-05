f48787da8732dc565686caae2bca3005b77d9c2e
Claudeilton Dantas <fcd007@hotmail.com>
2022-01-05 20:06:53 -0300

    chore: nova forma de add tasks Gulp

---
+++ README.md
@@ -0,0 +1,41 @@
+# Projeto Automação Front-End
+
+## RESUMO
+
+Automação de tarefas do dia a dia front-end:
+* Automação front end com npm. 
+* Tarefas no dia a dia utilizando Node.js e NPM. 
+* O gulp será o nosso principal aliado nesta tarefa.
+
+* Pré-Requisitos: HTML / CSS / JS
+
+## Descrição das tarefas automatizadas
+
+* Compilação de sass;
+* Adição automática de prefixos de browser;
+* Concatenação de arquivos;
+* Compressão de arquivos;
+* Transformar JavaScript moderno em JavaScript compatível com outros browsers e mais.
+
+## link do curso utilizado como base, excelente #origamid:
+* https://www.origamid.com/curso/automacao-front-end-com-npm/
+
+## Ferramentas utilizadas para embiente de desenvolvimento:
+* VS-Code;
+* NVM controle de versão pacotes Node;
+* NodeJS 14+;
+* Gulp 4.x.x;
+* Git.
+
+* Clone o projeto em um local com permissões,
+1. Execute no terminal ou bash o comando: `git clone https://github.com/fcd007/projeto-gulp.git` 
+2. Abra o projeto no seu editor de código preferido; 
+3. Dentro da pasta do projeto instale as depedências com o comando: `npm i ou npm install` e aguarde as dependências serem baixadas;
+4. Comandos úteis para executar:
+* `gulp --tasks` mostra todas as tarefas configuradas no projeto;
+* `gulp` comando padrão executa todos as tarefas do gulp e o servidor http;
+* `gulp plugins` comando para executar todos os plugins configurados com suas tarefas;
+* `gulp sass` executa as tarefas de SASS e CSS do projeto;
+* `gulp broswer-sync` executa servidor sincrono http do projeto;
+
+Agradeço a contribuição com feedback e sugestões, qualquer dúvida só enviar mensagem.
---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+//adiciona os plugins dos módulos instalados
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+
+//função para compilar o SASS e os prefixos
+function compilaSass() {
+  return gulp
+  .src('src/scss/**/*.scss')
+  .pipe(sass({
+    outputStyle: 'compressed'
+  })) //modo compresso
+  //.pipe(sass()) //modo padrão de saída - espaços e lixo junto
+  .pipe(gulp.dest('src/css'))
+  .pipe(autoprefixer({
+    cascade: false
+  }))
+  .pipe(browserSync.stream());
+}
+//gulp.task("sass", compilaSass); //executar como $gulp sass
+//tarefa de Gulp para a função de SASS
+// gulp.task('sass', function(done) {
+//   compilaSass();
+//   done();
+// }); // executa como padrão $ gulp
+
+//função para juntar arquivos .js
+function gulpJS() {
+  return gulp.src('src/js/main/**/*.js') //tenha o arquivo que gera loop - usar '!nomeFile.js'
+  .pipe(concat('main.js'))
+  .pipe(babel({
+    presets: ['@babel/preset-env']
+  }))
+  .pipe(uglify())
+  .pipe(gulp.dest('src/js'))
+  .pipe(browserSync.stream())
+}
+
+//tarefa para concat
+// gulp.task('mainjs', gulpJS);
+
+// JS Plugins
+function pluginJS() {
+  return gulp
+  .src([
+    'node_modules/jquery/dist/jquery.min.js',
+    'src/js/plugins/*.js'
+  ])
+  .pipe(concat('plugin.js'))
+  .pipe(gulp.dest('src/js'))
+  .pipe(browserSync.stream())
+}
+
+// gulp.task('pluginsjs', pluginJS);
+//função para inicia ro browser
+function browser() {
+  browserSync.init( {
+    server: {
+      baseDir: './src'
+    }
+  });
+}
+
+//tarefa do browser-sync
+// gulp.task('browser-sync', browser);
+
+//função de watch do Gulp
+function watch() {
+  gulp.watch('src/scss/**/*.scss', compilaSass);
+  gulp.watch('src/js/main/**/*.js', gulpJS);
+  gulp.watch('src/js/plugins/*.js', pluginJS);
+  gulp.watch(['src/*.html']).on('change', browserSync.reload);
+}
+
+//inicia a tarefa de watch
+// gulp.task('watch', watch);
+//tarefa padrão do Gulp
+gulp.task('default', gulp.parallel(watch,  browser, compilaSass, gulpJS, pluginJS));
+
+exports.compilaSass = compilaSass;
+exports.gulpJS = gulpJS;
+exports.pluginJS = pluginJS;
+exports.browser = browser;
+exports.watch = watch;
